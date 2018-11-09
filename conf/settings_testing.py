7b1a2250c2678981b1dfb1549553e25206c83cf2
pengwow <pengwow@hotmail.com>
2018-11-09 08:53:02 +0800

    修改数据库配置

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -16,13 +16,21 @@ from settings import APP_ID
 # ===============================================================================
 # 数据库设置, 测试环境数据库设置
 # ===============================================================================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+#         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+#         'USER': '',                            # 你的数据库user
+#         'PASSWORD': '',                        # 你的数据库password
+#         'HOST': '',                   		   # 数据库HOST
+#         'PORT': '3306',                        # 默认3306
+#     },
+# }
+from settings import APP_ID, PROJECT_ROOT
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
-        'PORT': '3306',                        # 默认3306
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+    }
 }
